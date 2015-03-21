be30f213dac0902f14f1ee8419698f6f10d00ecf
Owen Hurford <owen@hurfo.com>
2015-03-21 15:44:47 +0000

    Make config a sample file

---
+++ templates/config-sample.php
@@ -0,0 +1,31 @@
+<?php
+//Database configuration
+$dbhost = ''; //Database Host
+
+$dbport = ''; //Databse Host port number (Default if blank)
+
+$dbuser = ''; //Database Username (Must have full rights over database $dbname)
+
+$dbpass = ''; //Databse user password
+
+$dbname = ''; //Database Name
+
+
+//Organisation configuration
+$orgname = 'Sample'; //Organisation Name
+
+
+//Configuration for the letter to be sent
+$lettertitle = 'Test'; //Title to refer to letter when being seen by user
+
+$metatags = ''; //HTML Meta Tags for use on search engines.  If giving a social card then store images in the templates folder.
+
+$introduction = 'Testing'; //Introductory paragraph about the letter and why people should send it.  Can contain HTML.
+
+$confirm = 'Yes'; //Message to show on final screen before the user commits to sending.
+
+$sent = 'No!'; //Message to show after their message has been queued to be sent.
+
+//Letter template is stored in letter.json and is documented there
+
+?>
